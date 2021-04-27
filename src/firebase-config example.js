decfceac8bb8ffd02af4ed54053dae26734239a1
kevleop <kvalverde@unsa.edu.pe>
2021-04-27 01:20:50 -0500

    proteccion de datos

--- src/firebase-config example.js
+++ src/firebase-config example.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA9AdRDPbKGUtWVQZhs7hK1Ojdz06Yg4H8",
-  authDomain: "react-prueba-f5a9d.firebaseapp.com",
-  projectId: "react-prueba-f5a9d",
-  storageBucket: "react-prueba-f5a9d.appspot.com",
-  messagingSenderId: "318726860860",
-  appId: "1:318726860860:web:5235cb26e60e2bdca6d339",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
